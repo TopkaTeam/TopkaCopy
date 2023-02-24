d60c8bdbb9031e99e1ec6e696fb34be978cb9ae5
Danil Venikov <melongames@mail.ru>
2023-02-24 18:07:22 +0200

    Remove join message

--- src/main/java/ru/topka/copy/TopkaCopy.java
+++ src/main/java/ru/topka/copy/TopkaCopy.java
@@ -1,11 +1,8 @@
 package ru.topka.copy;
 
 import net.fabricmc.api.ModInitializer;
-import ru.topka.copy.thread.JoinMessageThread;
 
 public class TopkaCopy implements ModInitializer {
     @Override
-    public void onInitialize() {
-        new JoinMessageThread().start();
-    }
+    public void onInitialize() {}
 }
